8309877b8e91fe5392847af74f11dc11c841cdea
adunne8 <adunne08@gmail.com>
2019-02-11 23:39:08 +0000

    Tidy up and JS 1

---
+++ Section 10 - Intro to JS/10-2_js_stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Hello " + firstName + " " + lastName + ", you are " + age + " years old");
+console.log(`Hello ${firstName} ${lastName}, you are ${age} years old...AND THIS IS ES6!!!`);
---
+++ Section 10 - Intro to JS/10-3_age_calculator.js
@@ -0,0 +1,3 @@
+var age = prompt("How old are you?");
+var daysAlive = age*365.25;
+console.log(`You have been alive for: ${Math.floor(daysAlive)} ${daysAlive} days!`)
