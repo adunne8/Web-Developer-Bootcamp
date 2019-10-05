f91af10c5890bfcd3911f594cb27136f4b743f42
adunne8 <adunne08@gmail.com>
2019-10-05 18:47:34 +0100

    DOM1

---
+++ Section 15 - Intro to the DOM/15-1_select_and_manipulate.js
@@ -0,0 +1,22 @@
+var title = document.querySelector("h1");
+var titles = document.querySelectorAll("h1");
+var body = document.querySelector("body");
+var isBlue = false;
+
+for(i = 0; i < titles.length; i++){
+    titles[i].style.color = "green";
+}
+
+title.style.color = "red";
+titles[1].style.color = "blue";
+
+setInterval(function(){
+    if (isBlue){
+        body.style.backgroundColor = "white";
+    }
+    else{
+        body.style.backgroundColor = "#3498db";
+    }
+    isBlue = !isBlue;
+
+}, 1000)
