ef029c0f7e12dd88a2b8161f414f33bcfacc7b55
adunne8 <adunne08@gmail.com>
2019-11-27 22:47:20 +0000

    Finishing functions and styling

--- Section 17 - Colour Game/17-1_colour_game.js
+++ Section 17 - Colour Game/17-1_colour_game.js
@@ -1,17 +1,15 @@
-let colours = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-
-];
+let numSquares = 6;
+let colours = generateRandomColours(numSquares);
 
 let squares = document.querySelectorAll('.square');
 let colourDisplay = document.getElementById('colourdisplay');
 let pickedColour = pickColour();
 let messageDisplay = document.querySelector("#message");
+let header = document.querySelector('h1');
+let resetButton = document.querySelector('#reset');
+let easyBtn = document.getElementById("easybtn");
+let hardBtn = document.getElementById("hardbtn");
+
 
 
 colourDisplay.textContent = pickedColour;
@@ -24,9 +22,12 @@ function pickColour(){
 function compareColour() {
     let clickedColour = this.style.backgroundColor;
 
-    if(this.style.backgroundColor === pickedColour){
+    if(clickedColour === pickedColour){
         messageDisplay.textContent = "Correct!";
+        resetButton.textContent = "Play Again!"
         changeColours(pickedColour);
+        header.style.backgroundColor = pickedColour;
+
     }
     else {
         this.style.backgroundColor = '#232323';
@@ -35,11 +36,64 @@ function compareColour() {
 }
 
 function changeColours(colour){
-    console.log(colour);
-
     for(let i = 0; i < colours.length; i++){
         squares[i].style.backgroundColor = colour;
     }
+}
+
+
+function generateRandomColours(num){
+    // MAKE AN ARRAY
+    colourArray = [];
+
+    // RUN FOR REQUESTED num TIMES
+    for(let i = 0; i < num; i++){
+        // GENERATE A RANDOM COLOUR
+        colourArray.push(randomColour());
+    }
+   
+
+    return  colourArray;
+   
+}
+
+
+
+
+function randomColour(){
+    // PICK A RED FROM 0 - 255
+    let r = Math.floor(Math.random() * 256);
+    // PICK A GREEN FROM 0 - 255
+    let g = Math.floor(Math.random() * 256);
+    // PICK A BLUE FROM 0 - 255
+    let b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function resetFunction(){
+    // GENERATE NEW COLOURS
+    colours = generateRandomColours(numSquares);
+    // NEW RANDOM COLOUR
+    pickedColour = pickColour();
+    // CHNAGE COLOURS ON SQUARES
+
+    for(let i = 0; i < squares.length; i++){
+        // SET BACKGROUND COLOUR TO BE ONE OF THE ARRAY ENTRIES FOR EACH SQUARE
+        squares[i].style.backgroundColor = colours[i];
+    }
+
+    // CHANGE COLOUR TO CLICK ON
+    colourDisplay.textContent = pickedColour;
+
+    // CHANGE BACKGROUND COLOUR OF BANNER
+    header.style.backgroundColor = "steelblue";
+    
+    // CHANGE TEXT BACK TO STARTING TYPE
+    resetButton.textContent = "Reset"
+    messageDisplay.textContent = "Guess!";
+
+    
 }
 
 
@@ -50,4 +104,81 @@ for(let i = 0; i < squares.length; i++){
     squares[i].addEventListener('click', compareColour);
 }
 
+function makeEasy(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
 
+    numSquares = 3;
+
+    // GENERATE THREE COLOURS
+    colours = generateRandomColours(numSquares);
+    for(let i = 0; i < squares.length; i++){
+        // SET BACKGROUND COLOUR TO BE ONE OF THE ARRAY ENTRIES FOR EACH SQUARE
+        squares[i].style.backgroundColor = colours[i];
+    }
+
+    // PICK ONE
+    pickedColour = pickColour();
+    // UPDATE DISPLAY
+    colourDisplay.textContent = pickedColour;
+
+    // RESET PROGRESS
+    // CHANGE BACKGROUND COLOUR OF BANNER
+    header.style.backgroundColor = '#232323';
+
+    // CHANGE TEXT BACK TO STARTING TYPE
+    resetButton.textContent = "Reset"
+    messageDisplay.textContent = "Guess!";
+
+    for(let i = 0; i < squares.length; i++){
+        if(colours[i]){
+            squares[i].style.backgroundColor = colours[i];
+        }
+        else{
+            squares[i].style.display = "none";
+        }
+    }
+}
+
+
+function makeHard(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+
+    numSquares = 6;
+
+    
+    // GENERATE THREE COLOURS
+    colours = generateRandomColours(numSquares);
+    for(let i = 0; i < squares.length; i++){
+        // SET BACKGROUND COLOUR TO BE ONE OF THE ARRAY ENTRIES FOR EACH SQUARE
+        squares[i].style.backgroundColor = colours[i];
+    }
+
+    // PICK ONE
+    pickedColour = pickColour();
+    // UPDATE DISPLAY
+    colourDisplay.textContent = pickedColour;
+
+    // RESET PROGRESS
+    // CHANGE BACKGROUND COLOUR OF BANNER
+    header.style.backgroundColor = '#232323';
+
+    // CHANGE TEXT BACK TO STARTING TYPE
+    resetButton.textContent = "Reset"
+    messageDisplay.textContent = "Guess!";
+
+    for(let i = 0; i < squares.length; i++){
+        if(colours[i]){
+            squares[i].style.backgroundColor = colours[i];
+            squares[i].style.display = "block";
+        }
+        else{
+            squares[i].style.display = "none";
+        }
+    }
+}
+
+resetButton.addEventListener("click", resetFunction);
+easyBtn.addEventListener("click", makeEasy);
+hardBtn.addEventListener("click", makeHard);
