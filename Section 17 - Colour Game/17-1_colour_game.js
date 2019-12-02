24dd63f62b61cfcf67bd323bfadacf25f2b9aeb7
adunne8 <adunne08@gmail.com>
2019-12-02 23:02:18 +0000

    Completed Code Tidy

--- Section 17 - Colour Game/17-1_colour_game.js
+++ Section 17 - Colour Game/17-1_colour_game.js
@@ -1,18 +1,33 @@
 let numSquares = 6;
-let colours = generateRandomColours(numSquares);
+let colours = []; //generateRandomColours(numSquares);
+let pickedColour;
 
 let squares = document.querySelectorAll('.square');
 let colourDisplay = document.getElementById('colourdisplay');
-let pickedColour = pickColour();
 let messageDisplay = document.querySelector("#message");
 let header = document.querySelector('h1');
 let resetButton = document.querySelector('#reset');
-let easyBtn = document.getElementById("easybtn");
-let hardBtn = document.getElementById("hardbtn");
+//let easyBtn = document.getElementById("easybtn");
+//let hardBtn = document.getElementById("hardbtn");
 
+
+init();
+
+let modeButtons = document.querySelectorAll(".mode");
 
 
 colourDisplay.textContent = pickedColour;
+
+function init(){
+    for(let i = 0; i < squares.length; i++){
+        // SET BACKGROUND COLOUR TO BE ONE OF THE ARRAY ENTRIES FOR EACH SQUARE
+        //squares[i].style.backgroundColor = colours[i];
+        // ADD CALL TO FUNCTION WHEN CLICKED
+        squares[i].addEventListener('click', compareColour);
+        reset();
+    }
+    
+}
 
 function pickColour(){
     let randomNumner = Math.floor(Math.random() * colours.length);
@@ -51,12 +66,8 @@ function generateRandomColours(num){
         // GENERATE A RANDOM COLOUR
         colourArray.push(randomColour());
     }
-   
-
     return  colourArray;
-   
 }
-
 
 
 
@@ -71,16 +82,41 @@ function randomColour(){
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
-function resetFunction(){
+function changeDifficulty(){
+    // REMOVE selected CLASS FROM ALL BUTTONS THEN ADD TO CLICKED BUTTON
+    for(let i = 0; i < modeButtons.length; i++){
+        modeButtons[i].classList.remove("selected");
+    }
+    this.classList.add("selected");
+
+    if(this.textContent === "Easy"){
+        numSquares = 3;
+    }
+    else{
+        numSquares = 6;
+    }
+    reset();
+
+    
+}
+
+
+function reset(){
     // GENERATE NEW COLOURS
     colours = generateRandomColours(numSquares);
-    // NEW RANDOM COLOUR
+    // NEW RANDOM COLOUR TO SEARCH FOR
     pickedColour = pickColour();
-    // CHNAGE COLOURS ON SQUARES
 
+    // CHNAGE COLOURS ON SQUARES
     for(let i = 0; i < squares.length; i++){
         // SET BACKGROUND COLOUR TO BE ONE OF THE ARRAY ENTRIES FOR EACH SQUARE
-        squares[i].style.backgroundColor = colours[i];
+        if(colours[i]){
+            squares[i].style.backgroundColor = colours[i];
+            squares[i].style.display = "block";
+        }
+        else{
+            squares[i].style.display = "none";
+        }
     }
 
     // CHANGE COLOUR TO CLICK ON
@@ -91,94 +127,14 @@ function resetFunction(){
     
     // CHANGE TEXT BACK TO STARTING TYPE
     resetButton.textContent = "Reset"
-    messageDisplay.textContent = "Guess!";
-
-    
+    messageDisplay.textContent = "";
 }
 
 
-for(let i = 0; i < squares.length; i++){
-    // SET BACKGROUND COLOUR TO BE ONE OF THE ARRAY ENTRIES FOR EACH SQUARE
-    squares[i].style.backgroundColor = colours[i];
-    // ADD CALL TO FUNCTION WHEN CLICKED
-    squares[i].addEventListener('click', compareColour);
+resetButton.addEventListener("click", reset);
+//easyBtn.addEventListener("click", makeEasy);
+//hardBtn.addEventListener("click", makeHard);
+
+for(let i =0; i < modeButtons.length; i++){
+    modeButtons[i].addEventListener("click", changeDifficulty);
 }
-
-function makeEasy(){
-    easyBtn.classList.add("selected");
-    hardBtn.classList.remove("selected");
-
-    numSquares = 3;
-
-    // GENERATE THREE COLOURS
-    colours = generateRandomColours(numSquares);
-    for(let i = 0; i < squares.length; i++){
-        // SET BACKGROUND COLOUR TO BE ONE OF THE ARRAY ENTRIES FOR EACH SQUARE
-        squares[i].style.backgroundColor = colours[i];
-    }
-
-    // PICK ONE
-    pickedColour = pickColour();
-    // UPDATE DISPLAY
-    colourDisplay.textContent = pickedColour;
-
-    // RESET PROGRESS
-    // CHANGE BACKGROUND COLOUR OF BANNER
-    header.style.backgroundColor = '#232323';
-
-    // CHANGE TEXT BACK TO STARTING TYPE
-    resetButton.textContent = "Reset"
-    messageDisplay.textContent = "Guess!";
-
-    for(let i = 0; i < squares.length; i++){
-        if(colours[i]){
-            squares[i].style.backgroundColor = colours[i];
-        }
-        else{
-            squares[i].style.display = "none";
-        }
-    }
-}
-
-
-function makeHard(){
-    hardBtn.classList.add("selected");
-    easyBtn.classList.remove("selected");
-
-    numSquares = 6;
-
-    
-    // GENERATE THREE COLOURS
-    colours = generateRandomColours(numSquares);
-    for(let i = 0; i < squares.length; i++){
-        // SET BACKGROUND COLOUR TO BE ONE OF THE ARRAY ENTRIES FOR EACH SQUARE
-        squares[i].style.backgroundColor = colours[i];
-    }
-
-    // PICK ONE
-    pickedColour = pickColour();
-    // UPDATE DISPLAY
-    colourDisplay.textContent = pickedColour;
-
-    // RESET PROGRESS
-    // CHANGE BACKGROUND COLOUR OF BANNER
-    header.style.backgroundColor = '#232323';
-
-    // CHANGE TEXT BACK TO STARTING TYPE
-    resetButton.textContent = "Reset"
-    messageDisplay.textContent = "Guess!";
-
-    for(let i = 0; i < squares.length; i++){
-        if(colours[i]){
-            squares[i].style.backgroundColor = colours[i];
-            squares[i].style.display = "block";
-        }
-        else{
-            squares[i].style.display = "none";
-        }
-    }
-}
-
-resetButton.addEventListener("click", resetFunction);
-easyBtn.addEventListener("click", makeEasy);
-hardBtn.addEventListener("click", makeHard);
