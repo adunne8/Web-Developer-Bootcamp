8808ff0bdd9e6ca5a492528d1e695139a6222277
adunne8 <adunne08@gmail.com>
2019-12-17 20:45:29 +0000

    Methods

---
+++ Section 18 - jQuery/18-3_jquery_methods.js
@@ -0,0 +1,14 @@
+console.log($("#politics").attr("alt"));
+
+$("#politics").attr("alt", "Murica");
+
+$("#scenic").attr({
+    src: "https://i.imgur.com/3ndk61l.jpg",
+    alt: "Santa"
+});
+
+
+$("input:first-of-type").attr("type", "color");
+
+$("#lastname").val("LastName");
+console.log($("#lastname").val());
