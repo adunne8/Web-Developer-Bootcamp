64647b4600b369545dcdf024c2c0e5cd4e431ad4
adunne8 <adunne08@gmail.com>
2020-01-26 19:21:49 +0000

    Completed Methods and intro to jquery
    Completed attr, val, class manipulation
    Added jQuery intellisense config to project root

--- Section 18 - jQuery/18-3_jquery_methods.js
+++ Section 18 - jQuery/18-3_jquery_methods.js
@@ -9,6 +9,24 @@ $("#scenic").attr({
 
 
 $("input:first-of-type").attr("type", "color");
+$("input:first-of-type").attr("class", "colorBOx");
 
 $("#lastname").val("LastName");
 console.log($("#lastname").val());
+
+$("#dropdown").val("");
+console.log($("#dropdown").val());
+
+console.log($(".colorBOx").val());
+
+
+// CLASS MANIPULATION
+
+$("h1").addClass("correct");
+$("h1").removeClass("correct");
+
+$("ul li").addClass("wrong");
+$("ul li").removeClass("wrong");
+$("ul li").addClass("correct");
+$("ul li:first").addClass("done");
+$("ul li").toggleClass("done");
